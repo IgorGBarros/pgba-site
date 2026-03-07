c16ef8b412d7c73b768eb7230ac4c51fc05dfaa8
Igor Barros <igor.barros@oxaala.com>
2026-03-06 23:26:48 -0300

    fix: correção de configurações e arquivos atualizados

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -59,8 +59,7 @@ SECRET_KEY = 'django-insecure-+rc4szhd0d(k@((%vp6og@k_p)y5x*$9_=214d4+p@e3^o4gj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['gestao-estoque-k5vy.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
