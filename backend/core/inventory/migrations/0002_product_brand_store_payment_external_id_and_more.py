b939e9ea9f446a24ffb9209dfdc7d4e480ed2f09
Igor Guimarães Barros <igorguimaraesbarros@gmail.com>
2026-03-20 14:27:22 -0300

    fix:Correção RPA timeout

--- backend/core/inventory/migrations/0002_product_brand_store_payment_external_id_and_more.py
+++ backend/core/inventory/migrations/0002_product_brand_store_payment_external_id_and_more.py
@@ -3,7 +3,7 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
+class Migration(migrations.Migration): 
 
     dependencies = [
         ('inventory', '0001_initial'),
