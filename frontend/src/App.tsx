05c610b3e2efb992cedb5ed13cfbeabc4d2f453b
Igor Guimarães Barros <igorguimaraesbarros@gmail.com>
2026-04-12 23:32:34 -0300

    Initial commit - pgba site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Toaster as Sonner } from "../src/components/ui/sonner";
 import { TooltipProvider } from "../src/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../src/hooks/useAuth";
 
 
 
